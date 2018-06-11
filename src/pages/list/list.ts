eeb86bc1dff35231c2da64817204bd6f40609531
qianzd <qian.wolf@gmail.com>
2018-06-11 10:39:35 +0800

    delete adress

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -15,9 +15,9 @@ export class ListPage {
   goSearch(){
     console.log('key words:'+this.keyWords);
     if(this.keyWords==undefined){
-      this.path="http://open.boosj.com/search/video/by_keywords?category=1362&keywords=&size=20&page=1";
+      this.path="http://open.abc.com/search/video/by_keywords?category=1362&keywords=&size=20&page=1";
     }else{
-      this.path="http://open.boosj.com/search/video/by_keywords?category=1362&keywords="+this.keyWords+"&size=20&page=1";
+      this.path="http://open.abc.com/search/video/by_keywords?category=1362&keywords="+this.keyWords+"&size=20&page=1";
     }
     this.http.get(this.path).subscribe(data => {
       console.log(data);
