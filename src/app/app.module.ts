33f9f3aed07051f25791db219b68bf06c40c5b49
qianzd <qian.wolf@gmail.com>
2018-06-11 10:32:54 +0800

    http请求

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import {ListPage} from "../pages/list/list";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 import {NavController} from "ionic-angular";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   templateUrl: 'list.html'
 })
 export class ListPage {
-  constructor(public navCtrl: NavController) {
+  keyWords:string;
+  path:string;
+
+  constructor(public navCtrl: NavController,private http: HttpClient) {
     console.log('List Page Constructor initialization!');
+  }
+  goSearch(){
+    console.log('key words:'+this.keyWords);
+    if(this.keyWords==undefined){
+      this.path="http://open.boosj.com/search/video/by_keywords?category=1362&keywords=&size=20&page=1";
+    }else{
+      this.path="http://open.boosj.com/search/video/by_keywords?category=1362&keywords="+this.keyWords+"&size=20&page=1";
+    }
+    this.http.get(this.path).subscribe(data => {
+      console.log(data);
+      this.showList(data);
+    });
+  }
+  showList(data:any){
   }
 }
